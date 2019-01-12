4abf4d851b9a8d2be5e24705547587ead6721225
zhudaoruyi <1430757558@qq.com>
2019-01-13 00:34:49 +0800

    [update]

--- run1.sh
+++ run1.sh
@@ -4,8 +4,8 @@
 # h=720
 
 # 同时启动，分号：顺序地独立执行各条命令，不管是否失败
-python snapshots.py --video_device 0 --prefix test0 --width ${w} --height ${h} ;
-python snapshots.py --video_device 1 --prefix test0 --width ${w} --height ${h} ;
-python snapshots.py --video_device 2 --prefix test0 --width ${w} --height ${h} ;
+python snapshots.py --video_device 0 --prefix test0 --width ${w} --height ${h} &
+python snapshots.py --video_device 1 --prefix test0 --width ${w} --height ${h} &
+python snapshots.py --video_device 2 --prefix test0 --width ${w} --height ${h} &
 python snapshots.py --video_device 3 --prefix test0 --width ${w} --height ${h}
 
